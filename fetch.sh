14035d985823b3281f5859a3b4676b299d8e451e
Shinta Koyanagi <yancya@upec.jp>
2017-01-22 15:24:44 +0900

    Add fetch script

---
+++ fetch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl https://cloud.google.com/bigquery/docs/reference/legacy-sql > legacy_sql/legacy_sql.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/ > standard_sql/index.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/enabling-standard-sql > standard_sql/enabling_standard_sql.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/migrating-from-legacy-sql > standard_sql/migrating_from_legacy_sql.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/data-types > standard_sql/data_types.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/query-syntax > standard_sql/query_syntax.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/functions-and-operators > standard_sql/functions_and_operators.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/lexical  > standard_sql/lexical.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/arrays > standard_sql/arrays.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/user-defined-functions > standard_sql/user_defined_functions.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/data-manipulation-language > standard_sql/data_manipulation_language.html
+curl https://cloud.google.com/bigquery/docs/reference/standard-sql/dml-syntax > standard_sql/dml_syntax.html
